f71b0f144138ba266b1b0c7f7a4c84edd0815907
sjurwold <sjur_wold@hotmail.com>
2019-02-20 20:41:54 +0100

    URL

--- eventify/event/urls.py
+++ eventify/event/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='eventify-home'),
-    path('about/', views.about, name='eventify-about')
+    path('', views.HtmlRender.home, name='eventify-home'),
+    path('about/', views.HtmlRender.about, name='eventify-about'),
+    path('createEvent/', views.HtmlRender.createEvent, name='create-event'),
+    path('allEvents/', views.HtmlRender.allEvents, name='all-events'),
+    path('myEvents/', views.HtmlRender.myEvents, name='my-events'),
+    path('editEvent/', views.HtmlRender.editEvent, name='edit-event'),
+    path('event/update/<int:post_id>/', views.EventViews.updateEvent, name='update-event'),
+    path('event/remove/', views.EventViews.removeEvent, name='remove-event'),
+    path('event/create/', views.EventViews.createEvent, name='createEvent'),
+    path('event/search/', views.EventViews.searchEvents, name='searchEvents'),
+    path('event/details/', views.EventViews.eventDetails, name='eventDetails'),
+    path('event/join/', views.EventViews.eventJoin, name='eventJoin'),
 ]
