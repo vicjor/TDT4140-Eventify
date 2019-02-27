61aa67ee9163f192c41074c886ab560041623c39
Victor Jørgensen <victojo@stud.ntnu.no>
2019-02-27 19:23:00 +0100

    Fikset innlogging

--- eventify/event/views.py
+++ eventify/event/views.py
@@ -38,7 +38,7 @@ class EventDetailView(DetailView):
 
 class EventCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content', 'attendance_limit']
+    fields = ['title', 'content', 'attendance_limit', 'start_date', 'end_date']
     template_name = 'event/event_form.html'
 
     def form_valid(self, form):
@@ -47,7 +47,7 @@ class EventCreateView(LoginRequiredMixin, CreateView):
 
 class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content', 'attendance_limit']
+    fields = ['title', 'content', 'attendance_limit', 'start_date', 'end_date']
     template_name = 'event/event_form.html'
     context_object_name = 'events'
 
@@ -60,6 +60,8 @@ class EventUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == event.author:
             return True
         return False
+
+
 #END
 
 class EventDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
@@ -122,7 +124,7 @@ class HtmlRender:
             'events': events
         }
 
-        return render(request, 'event/allEvents.html', context)
+        return render(request, 'event/all_events.html', context)
 
     @login_required
     def myEvents(request):
@@ -143,7 +145,7 @@ class HtmlRender:
             'joined_events': joined_events
         }
 
-        return render(request, 'event/myEvents.html', context)
+        return render(request, 'event/my_events.html', context)
 
     @login_required
     def editEvent(request, event_id):
@@ -303,10 +305,10 @@ class EventViews:
         attendance = event.attendees.all().count()
 
         # create response
-        if attendance + 1 <= event.attendance_limit:
+        if attendance + 1 >= event.attendance_limit:
             messages.error(request, f'The event is already full.')
-            return render(request, )
-        elif len(event.attendees.filter(attendees__id=user.id)) == 1:
+
+        elif event.attendees.filter(attendees__id=user.id) == 1:
             messages.error(request, f'You are already signed up for this event.')
         else:
             # add user to event
