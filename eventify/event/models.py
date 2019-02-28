6ae5d1254caecaf2f45050016b22c37c4ca6b07e
Ola Holde <olahold@stud.ntnu.no>
2019-02-28 11:26:17 +0100

    Private events vises ikke på listen med events

--- eventify/event/models.py
+++ eventify/event/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     location = models.CharField(max_length=100, null=True)
     content = models.TextField()
     image = models.ImageField(default="default.jpg", upload_to="event_images")
-
+    is_private = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
