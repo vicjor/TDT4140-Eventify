0324df67964608a32420380bed010da8f8a452bc
Eline <elinego@stud.ntnu.no>
2019-03-13 15:26:15 +0100

    Database-oppdatering for notifications

--- eventify/users/models.py
+++ eventify/users/models.py
@@ -22,14 +22,14 @@ class Profile(models.Model):
 
 class Notification(models.Model):
     user = models.ForeignKey(Profile, on_delete = models.CASCADE) #Hvem som mottar varselet
-    title = models.CharField(max_length=100) #Text i varselet
+    text = models.CharField(max_length=300) #Text i varselet
     time = models.DateTimeField() #Når man fikk varselet
     read = models.BooleanField(default=False) #har de sett varselet enda?
     link = models.URLField() #Link til side varselet omhandler
     sender = models.CharField(max_length=100) #Hvem/Hva utløste varselet?
 
     def __str__(self):
-        return self.title
+        return self.text
 
 
 
