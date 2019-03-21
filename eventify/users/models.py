ebfdf667c6ec3634f421c93bdc58394f2b840d32
sjurwold <sjur_wold@hotmail.com>
2019-03-21 12:17:10 +0100

    Notifications, separated in different types, missing front-end

--- eventify/users/models.py
+++ eventify/users/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from event.models import Post
+from django.utils import timezone
 from PIL import Image #Brukes for bildehåndtering / profilbilde
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #Hvem som mottar varselet
+    event = models.ForeignKey(Post, on_delete=models.CASCADE, default=Post.objects.first())
+    text = models.CharField(max_length=300) #Text i varselet
+    time = models.DateTimeField(default=timezone.now) #Når man fikk varselet
+    read = models.BooleanField(default=False) #har de sett varselet enda?
+    link = models.URLField() #Link til side varselet omhandler
+    sender = models.CharField(max_length=100) #Hvem/Hva utløste varselet?
+    type = models.CharField(max_length=10, default="profile")
+
+    def __str__(self):
+        return self.text
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #Er en 1-1 relasjon mellom bruker og profil
@@ -10,6 +26,8 @@ class Profile(models.Model):
     requests = models.ManyToManyField(User, related_name="requests")
     event_invites = models.ManyToManyField(Post, related_name="event_invites")
     sent_requests = models.ManyToManyField(User, related_name="sent_requests")
+    notifications = models.ManyToManyField(Notification, related_name="notifications")
+    new_notifications = models.IntegerField(default=0)
 
 
     def __str__(self):
@@ -25,16 +43,9 @@ class Profile(models.Model):
             img.save(self.image.path)
 
 
-class Notification(models.Model):
-    user = models.ForeignKey(Profile, on_delete = models.CASCADE) #Hvem som mottar varselet
-    text = models.CharField(max_length=300) #Text i varselet
-    time = models.DateTimeField() #Når man fikk varselet
-    read = models.BooleanField(default=False) #har de sett varselet enda?
-    link = models.URLField() #Link til side varselet omhandler
-    sender = models.CharField(max_length=100) #Hvem/Hva utløste varselet?
 
-    def __str__(self):
-        return self.text
+
+
 
 
 
--- eventify/users/views.py
+++ eventify/users/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .models import Notification
 from django.contrib import messages
 from django.urls import reverse
 from django.db.models import Q
@@ -76,6 +77,14 @@ def add_contact(request):
     user.profile.requests.add(request.user)
     request.user.profile.sent_requests.add(user)
 
+    notification = Notification.objects.create(
+        user=user,
+        text='{} {} sent you a contact request'.format(request.user.first_name, request.user.last_name),
+        type="profile"
+    )
+
+    user.profile.notifications.add(notification)
+
     messages.info(request, f'Request sent. ')
 
     return HttpResponseRedirect(reverse('all-users'))
@@ -103,6 +112,14 @@ def accept_request(request):
     request.user.profile.requests.remove(user)
     request.user.profile.contacts.add(user)
 
+    notification = Notification.objects.create(
+        user=user,
+        text='{} {} accepted your contact request.'.format(request.user.first_name, request.user.last_name),
+        type="profile"
+    )
+
+    user.profile.notifications.add(notification)
+
     messages.success(request, f'Request has been accepted. ')
 
     return HttpResponseRedirect(reverse('contact-requests'))
@@ -117,6 +134,14 @@ def decline_request(request):
 
     request.user.profile.requests.remove(user)
     messages.info(request, f'Request has been declined. ')
+
+    notification = Notification.objects.create(
+        user=user,
+        text='{} {} declined your contact request.'.format(request.user.first_name, request.user.last_name),
+        type="profile"
+    )
+
+    user.profile.notifications.add(notification)
 
     return HttpResponseRedirect('contact-requests')
 
@@ -194,4 +219,3 @@ def event_invites(request):
     }
 
     return render(request, 'users/event_invites.html', context)
-
