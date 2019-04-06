4ed7fdb74c6ec920128c6c856640cf04d4b1ace8
Eline <elinego@stud.ntnu.no>
2019-04-06 14:30:16 +0200

    tror det var testen som ikke funka, prøver å fjerne en test

--- eventify/users/tests.py
+++ eventify/users/tests.py
@@ -3,34 +3,3 @@ from .models import User, Profile
 from PIL import Image
 from django.test import Client
 from unittest.mock import patch
-
-# Create your tests here.
-
-
-@classmethod
-def setUpClass(cls):
-    pass
-
-
-class ModelTestCase(TestCase):
-    def setUp(self):
-        print("Setting up: UserTestCase")
-        self.user1 = User.objects.create_user(username='Ole', email='ole@mail.no', password='ole')
-        self.user2 = User.objects.create_user(username='Sjur', email='sjur@mail.no', password='sjur')
-        self.profile1 = Profile.objects.create_profile(self.user1)
-        self.profile2 = Profile.objects.create_profile(self.user2)
-
-    def test_profile(self):
-        self.assertTrue(len(Profile.objects.filter(username='Sjur')) == 1)
-        self.assertTrue(len(Profile.objects.filter(username='Ole')) == 1)
-
-    def test_profile_to_string(self):
-        self.assertEqual(str(self.profile1), self.profile1.text)
-
-    def test_profile_pre_save_hook(self):
-        img = Image.new('RGB', (400, 400), color='red')
-        img.save('pil_red.png')
-        self.profile1.image = img
-        self.profile1.save()
-        self.assertTrue(Profile.objects.filter(image="pil_red.png").exists())
-        self.assertEqual(self.profile1.image.size, (300, 300))
